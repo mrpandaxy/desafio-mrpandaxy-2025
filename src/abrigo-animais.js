4242cbbeb38ca0d17dda264e99645aa8efb5b9eb
vinicius <vini.r.faria@hotmail.com>
2025-09-05 16:16:41 -0300

    feat: adiciona lista oficial de animais do abrigo e valida se ordem recebida contém apenas animais válidos, sem duplicados

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -1,6 +1,25 @@
 class AbrigoAnimais {
 
   encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
+      
+ // lista oficial dos animais
+    const animaisDoAbrigo = ["Rex","Mimi","Fofo","Zero","Bola","Bebe","Loco"];
+
+    // transforma a string ordemAnimais (parâmetro) em um array
+    const ordemDosAnimaisArray = ordemAnimais.split(",");
+    
+    // validar se os animais existem no abrigo e se estão duplicados
+    const animaisVistos = new Set();
+    for (const animal of ordemDosAnimaisArray) {
+      if (!animaisDoAbrigo.includes(animal) || animaisVistos.has(animal)) {
+        return { erro: `Animal inválido: ${animal}` }; 
+      }
+
+      animaisVistos.add(animal);
+    }
+
+    // por enquanto só retorna lista vazia para os testes avançarem
+    return { lista: [0], erro: null };
   }
 }
 
