aece64cc6d67145e990d8d868c10077415a80cdc
vinicius <vini.r.faria@hotmail.com>
2025-09-09 10:59:13 -0300

    implementando os brinquedos do abrigo.

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -5,19 +5,38 @@ class AbrigoAnimais {
  // lista oficial dos animais
     const animaisDoAbrigo = ["Rex","Mimi","Fofo","Zero","Bola","Bebe","Loco"];
 
-    // transforma a string ordemAnimais (parâmetro) em um array
+    //lista oficial dos brinquedos
+    const brinquedosDoAbrigo =["RATO","BOLA","LASER","CAIXA","NOVELO","SKATE"];
+
+    // convertendo string de animais em arrays
     const ordemDosAnimaisArray = ordemAnimais.split(",");
     
+
     // validar se os animais existem no abrigo e se estão duplicados
     const animaisVistos = new Set();
     for (const animal of ordemDosAnimaisArray) {
       if (!animaisDoAbrigo.includes(animal) || animaisVistos.has(animal)) {
         return { erro: `Animal inválido` }; 
       }
-
       animaisVistos.add(animal);
     }
+    
+    //convertendo string de brinquedos em arrays e tirando espaços extras
+    const brinquedosPessoa1Array = brinquedosDoAbrigo.split(",").map(item => item.trim());
+    const brinquedosPessoa2Array = brinquedosDoAbrigo.split(",").map(item => item.trim());
 
+    function validarBrinquedos(listaBrinquedos) {
+      const brinquedosVistos = new Set();
+
+      for(const brinquedos of listaBrinquedos){
+        if(!brinquedosDoAbrigo.includes(brinquedos || )){
+          
+        }
+      }
+      
+    }
+
+    //validar
     
 
   }
